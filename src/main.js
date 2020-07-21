417338bc016f58d693f7656070250172291d99b2
Rebornjiang <626104624@qq.com>
2020-07-21 23:13:13 +0800

    表单封装

--- src/main.js
+++ src/main.js
@@ -8,10 +8,11 @@ import http from '@/config/axios'
 
 Vue.config.productionTip = false
 // 配置所有的element ui 组件的大小尺寸
-Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
 // 所有请求方法挂载到原型上
 Vue.prototype.$axios = http
 
+console.log(Vue, '看看')
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="home">
     <h1>demo</h1>
-    <div style="width: 500px;margin: 0 auto; border: 1px solid #ccc; height: 500px;">
-      <jr-form refName="testForm" :formConfig="formConfig" :formData="formData" :rules="rules"> </jr-form>
+    <div style="width: 500px;margin: 0 auto; border: 1px solid #ccc;padding: 20px;">
+      <jr-form
+        refName="testForm"
+        :formConfig="formConfig"
+        :formData="formData"
+        :rules="rules"
+        @dateChange="dateChange"
+        @radioGroupChange="radioGroupChange"
+        @selectChange="selectChange"
+      >
+      </jr-form>
     </div>
   </div>
 </template>
@@ -16,7 +25,7 @@ export default {
   components: { JrForm },
   data() {
     return {
-      formData: {},
+      formData: { selectVal: [1, 2], dateTimeVal: ['2020-06-30 12:00:00', '2020-07-30 12:00:00'], dateVal: ['2020-06-30', '2020-07-30'], checkboxVal: [1, 2], testVal: 'reborn', readOnlyVal: 'hello world', passwordVal: '1234541', textareaVal: 'jalsdkjfalkdsjf' },
       rules: {},
       formConfig: [
         {
@@ -24,8 +33,100 @@ export default {
           list: [
             {
               type: 'text',
+              beforeTxt: 'hello',
               prop: 'testVal',
-              label: '文本类型'
+              label: '文本类型',
+              readOnly: false,
+              style: { textAlign: 'right', fontSize: '30px' }
+            },
+            {
+              type: 'readOnly',
+              prop: 'readOnlyVal',
+              label: '只读文本',
+              afterTxt: '哈哈哈'
+            },
+            {
+              type: 'password',
+              prop: 'passwordVal',
+              label: '密码'
+            },
+            {
+              type: 'textarea',
+              prop: 'textareaVal',
+              label: '文本域',
+              readOnly: false
+            },
+            {
+              type: 'radioGroup',
+              prop: 'radioGroupVal',
+              label: '单选框组',
+              optionList: [
+                { label: '吃饭', value: 1 },
+                { label: '睡觉', value: 2 }
+              ],
+              changeFn: 'radioGroupChange'
+            },
+            {
+              type: 'radioItem',
+              prop: 'radioItemVal',
+              label: '单项单选'
+            },
+            {
+              type: 'checkbox',
+              prop: 'checkboxVal',
+              label: '多选框',
+              optionList: [
+                { label: 'reborn', value: 1 },
+                { label: 'jiang', value: 2 },
+                { label: 'tong', value: 3 }
+              ],
+              changeFn: 'jfslkdjk',
+              readOnly: false
+            },
+            {
+              type: 'date',
+              prop: 'dateVal',
+              label: '日期选择器',
+              readOnly: false,
+              dateConfig: {
+                disabledDate(time) {
+                  const oneday = 86400
+                  const before = Date.now() - oneday * 5
+                  // console.log(before < Date.now())
+                  return time.getTime() >= before
+                }
+              },
+              changeFn: 'dateChange',
+              datePickerType: 'datetimerange'
+            },
+            {
+              type: 'dateTime',
+              prop: 'dateTimeVal',
+              label: '日期时间选择',
+              readOnly: false,
+              changeFn: 'dateChange'
+            },
+            {
+              type: 'select',
+              prop: 'selectVal',
+              label: '单项或多选',
+              readOnly: false,
+              changeFn: 'selectChange',
+              multiple: true,
+              optionList: [
+                {
+                  label: '香蕉',
+                  value: 1
+                },
+                {
+                  label: '苹果',
+                  value: 2
+                },
+                {
+                  label: '波罗蜜',
+                  value: 3
+                }
+              ]
             }
           ]
         }
@@ -39,6 +140,15 @@ export default {
     async loginInfo() {
       const res = await this.$axios.login()
       console.log(res, 'resss')
+    },
+    radioGroupChange(val) {
+      console.log('radioGroupChange改变了')
+    },
+    dateChange(val) {
+      console.log(val, '选择了date了')
+    },
+    selectChange(val) {
+      console.log(val, 'select 选择')
     }
   }
 }
